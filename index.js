0eb1204413acdbfddee76d76e0c06926818ca82a
elementxfordev <elementxfordev@gmail.com>
2023-10-31 12:21:14 +0530

    changed

--- index.js
+++ index.js
@@ -29,19 +29,18 @@ mongoose.connect('mongodb+srv://karanprajapat824:karanprajapat824@cluster0.xescp
 const userSchema = new mongoose.Schema({
     username : String,
     gmail : String,
-    password : String
+    password : String,
 });
 
 const contentSchema = new mongoose.Schema({
     html : String,
     css : String,
-    js : String,
     category : String,
     username : String,
-    postnumber : Number
+    _id : String
 });
 
-const UserData = mongoose.model('UserData',contentSchema);
+const Content = mongoose.model('content',contentSchema);
 const User = mongoose.model('websiteUser',userSchema);
 
 app.use(bodyParser.json());
@@ -51,6 +50,7 @@ app.post('/register',async (request,response) => {
     const username = request.body.username;
     const gmail = request.body.gmail;
     const password = request.body.password;
+    const image = request.body.image;
     let existingUser = await User.findOne({username});
     if(existingUser)
     {
@@ -61,7 +61,7 @@ app.post('/register',async (request,response) => {
     {
         return response.json({message : "Gmail is already exist"});
     }
-    const user = {username,password,gmail};
+    const user = {username,password,gmail,image};
     const newUser = new User(user);
     newUser.save();
     const token = jwt.sign(user,secret);
@@ -94,20 +94,18 @@ app.post('/login',async (request,response) => {
 app.post('/create',verify,(request,response)=>{
     const html = request.body.html;
     const css = request.body.css;
-    const js = request.body.js;
     const category = request.body.category;
     const userid = request.body.userid;
-    const postnumber = request.body.postnumber;
-    const userdata = new UserData({html,css,js,category,userid,postnumber});
-    userdata.save();
+    const newContent = new Content({html,css,category,userid});
+    newContent.save();
     return response.json({message : "your creation saved successfully",
-    html,css,js,category,userid,postnumber});
+    html,css,category,userid});
 });
 
 app.get('/getall',async (request,response) => {
     try
     {
-        const data = await UserData.find({});
+        const data = await Content.find({});
         return response.json({data});
     }catch(error)
     {
@@ -118,30 +116,27 @@ app.get('/getall',async (request,response) => {
 app.post('/update',verify,async (request,rsponse)=>{
     const html = request.body.html;
     const css = request.body.css;
-    const js = request.body.js;
     const category = request.body.category;
-    const username = request.body.userid;
-    const postnumber = request.body.postnumber;
-    let existingData = await UserData.findOne({postnumber});
+    const userid = request.body.userid;
+    const _id = request.body.id;
+    let existingData = await Content.findOne({_id});
     if(!existingData)
     {
         return express.response.json({message : "User not found"});
     }
     existingData.html = html;
     existingData.css = css;
-    existingData.js = js;
     existingData.category = category;
-    existingData.userid = userid;
     await existingData.save();
 });
 
 app.delete('/delete',verify,async (request,response) => {
-    const postnumber = request.body.postnumber;
+    const id = request.body.id;
     const userid = request.body.userid;
-    let existingData = await UserData.findOne({userid});
+    let existingData = await Content.findOne({userid});
     if(existingData)
     {
-        let existingPost = await UserData.findOneAndDelete({postnumber});
+        let existingPost = await Content.findOneAndDelete({id});
         if(!existingPost)
         {
             return response.json({message : "post not found"});
@@ -149,6 +144,51 @@ app.delete('/delete',verify,async (request,response) => {
         return response.json({message : "post deleted successfuly"});
     }
     return response.json({message : "invalid user id"});
+});
+
+app.get('/getButtons',async (request,response) =>{
+    const data = await Content.find({category : "Buttons"});
+    response.json({data});
+});
+
+app.get('/getForms',async (request,response) =>{
+    const data = await Content.find({category : "Forms"});
+    response.json({data});
+});
+
+app.get('/getCards',async (request,response) =>{
+    const data = await Content.find({category : "Cards"});
+    response.json({data});
+});
+
+app.get('/getCheckBoxes',async (request,response) =>{
+    const data = await Content.find({category : "CheckBoxes"});
+    response.json({data});
+});
+
+app.get('/getInputs',async (request,response) =>{
+    const data = await Content.find({category : "Inputs"});
+    response.json({data});
+});
+
+app.get('/getLoaders',async (request,response) =>{
+    const data = await Content.find({category : "Loaders"});
+    response.json({data});
+});
+
+app.get('/getRadioButtons',async (request,response) =>{
+    const data = await Content.find({category : "RadioButtons"});
+    response.json({data});
+});
+
+app.get('/getToggleSwitches',async (request,response) =>{
+    const data = await Content.find({category : "ToggleSwitches"});
+    response.json({data});
+});
+
+app.get('/getOthers',async (request,response) =>{
+    const data = await Content.find({category : "Others"});
+    response.json({data});
 });
 
 
