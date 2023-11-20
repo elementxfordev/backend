b0392970f500ee5d7713fb9fbf21d3853ec0a315
elementxfordev <elementxfordev@gmail.com>
2023-11-20 16:36:05 +0530

    New thingds

--- index.js
+++ index.js
@@ -26,31 +26,44 @@ mongoose.connect('mongodb+srv://karanprajapat824:karanprajapat824@cluster0.xescp
     useNewUrlParser : true,
     useUnifiedTopology : true
 });
+
 const userSchema = new mongoose.Schema({
     username : String,
     gmail : String,
     password : String,
 });
 
+
 const contentSchema = new mongoose.Schema({
     html : String,
     css : String,
     category : String,
     username : String,
-    _id : String
 });
 
 const Content = mongoose.model('content',contentSchema);
 const User = mongoose.model('websiteUser',userSchema);
 
+
+
 app.use(bodyParser.json());
 app.use(cors());
 
+app.post('/userInfo',verify,async (request,response)=>{
+    const username = request.body.username;
+    console.log(username);
+    const data = await Content.find({username : username});
+    if(data)
+    {
+        response.status(200).json({data});
+    }
+})
+
 app.post('/register',async (request,response) => {
+    const fullname = request.body.fullname;
     const username = request.body.username;
     const gmail = request.body.gmail;
     const password = request.body.password;
-    const image = request.body.image;
     let existingUser = await User.findOne({username});
     if(existingUser)
     {
@@ -61,7 +74,7 @@ app.post('/register',async (request,response) => {
     {
         return response.json({message : "Gmail is already exist"});
     }
-    const user = {username,password,gmail,image};
+    const user = {username,password,gmail,fullname};
     const newUser = new User(user);
     newUser.save();
     const token = jwt.sign(user,secret);
@@ -95,29 +108,32 @@ app.post('/create',verify,(request,response)=>{
     const html = request.body.html;
     const css = request.body.css;
     const category = request.body.category;
-    const userid = request.body.userid;
-    const newContent = new Content({html,css,category,userid});
+    const username = request.body.username;
+    const newContent = new Content({html,css,category,username});
     newContent.save();
-    return response.json({message : "your creation saved successfully",
-    html,css,category,userid});
+    return response.status(200).json({message : "your creation saved successfully",
+    html,css,category,username});
 });
 
-app.get('/getall',async (request,response) => {
-    try
-    {
-        const data = await Content.find({});
-        return response.json({data});
-    }catch(error)
-    {
-        return response.json({message : "error"});
+app.get('/getall', async (request, response) => {
+    try {
+      const pageSize = 15;
+  
+      const data = await Content.aggregate([
+        { $match: { category: { $nin: ["Forms", "Cards"] } } },
+        { $sample: { size: pageSize } },
+     ]);
+
+      return response.status(200).json({ data });
+    } catch (error) {
+      console.error(error);
+      return response.status(500).json({ message: 'Internal Server Error' });
     }
-})
+  });
 
 app.post('/update',verify,async (request,rsponse)=>{
     const html = request.body.html;
     const css = request.body.css;
-    const category = request.body.category;
-    const userid = request.body.userid;
     const _id = request.body.id;
     let existingData = await Content.findOne({_id});
     if(!existingData)
@@ -126,17 +142,15 @@ app.post('/update',verify,async (request,rsponse)=>{
     }
     existingData.html = html;
     existingData.css = css;
-    existingData.category = category;
     await existingData.save();
 });
 
 app.delete('/delete',verify,async (request,response) => {
-    const id = request.body.id;
-    const userid = request.body.userid;
-    let existingData = await Content.findOne({userid});
+    const _id = request.body._id;
+    let existingData = await Content.findOne({_id});
     if(existingData)
     {
-        let existingPost = await Content.findOneAndDelete({id});
+        let existingPost = await Content.findOneAndDelete({_id});
         if(!existingPost)
         {
             return response.json({message : "post not found"});
@@ -147,7 +161,13 @@ app.delete('/delete',verify,async (request,response) => {
 });
 
 app.get('/getButtons',async (request,response) =>{
-    const data = await Content.find({category : "Buttons"});
+    const pageSize = 15;
+  
+      const data = await Content.aggregate([
+        { $match: { category: "Buttons" } },
+        { $sample: { size: pageSize } },
+     ]);
+
     response.json({data});
 });
 
@@ -191,6 +211,19 @@ app.get('/getOthers',async (request,response) =>{
     response.json({data});
 });
 
+app.post('/getElementById', async (request, response) => {
+    const _id = request.body._id;
+    const data = await Content.find({_id : _id});
+    if(data)
+    {
+        response.status(200).json({data});
+    }
+    else 
+    {
+        response.json({message : "Element not found"});
+    }
+    
+  });
 
 app.listen(port,()=>{
     console.log(`Listening on port number ${port}`);
